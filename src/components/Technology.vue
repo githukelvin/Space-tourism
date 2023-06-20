dee6e630bf64a9d6111a1b18f6d9fe7b2d044a18
githukelvin <kelvingithu019@gmail.com>
2023-06-20 23:20:52 +0300

    build goes on

---
+++ src/components/Technology.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="content">
+        <div class="left">
+            <div class="sideBar">
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </div>
+            <div class="info">
+                <h4>the terminology</h4>
+                <h1>launch vehicle</h1>
+                <p>Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Quidem tempore placeat 
+                    explicabo nesciunt harum repudiandae nulla 
+                    officia provident minima non, doloribus 
+                    recusandae iusto, odio illo eaque distinctio 
+                    nihil fugit doloremque, molestias ab! Eum, 
+                    consectetur quos? Libero, sint facilis? At,
+                     sequi sit! Facilis cupiditate labore temporibus nisi. Debitis saepe sapiente ipsum, magnam tempore quas pariatur perferendis laudantium, qui, in nam.</p>
+            </div>
+
+        </div>
+        <div class="right">
+            <img src="../assets/technology/image-launch-vehicle-portrait.jpg" alt="">
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
